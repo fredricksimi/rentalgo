7c9bab7bf8f10de017c8c0ac1c12c0654c1fbb64
Fredrick Simi <fkemosi@gmail.com>
2022-12-08 21:07:51 +0300

    other changes

--- myapp/app/rentalgo/settings.py
+++ myapp/app/rentalgo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p#hfim2*dh*bf#dom5q3lp+so&=ezt^mu98!6)iii$vz@ie_)t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
