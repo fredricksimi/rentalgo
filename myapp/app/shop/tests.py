376ded52ed3762a99c1c539d788f7348a955addb
Fredrick Simi <fkemosi@gmail.com>
2022-12-10 11:32:59 +0300

    added tests

--- myapp/app/shop/tests.py
+++ myapp/app/shop/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from shop.models import Product, Category
+from django.utils import timezone
+from users.models import CustomUser
 
-# Create your tests here.
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        Category.objects.create(
+            name = 'Fashion',
+            slug = 'fashion',
+            created_at = timezone.now(),
+            updated_at = timezone.now()
+        )
+
+    def test_model(self):
+        cat = Category.objects.get(id=1)
+        self.assertEqual(cat.name, 'Fashion')
+
+class ProductTestCase(TestCase):
+    def setUp(self):
+        
+        self.user = CustomUser.objects.create(username='testUser', email='test@email.com', first_name='test', last_name='user', password='')
+        self.user.set_password('secret')
+        self.user.save()       
+        Category.objects.create(name='Fashion', slug='fashion', created_at=timezone.now(), updated_at=timezone.now())
+        Product.objects.create(
+            owner=self.user,
+            category=Category.objects.get(id=1),
+            name='Hat', slug='hat',description='Amazing hat',
+            price=49.99, available=True, rent_duration='One Day', 
+            created_at=timezone.now(), updated_at=timezone.now(), 
+            image=None
+        )
+
+    def test_contact_model(self):
+        item = Product.objects.get(id=1)
+        self.assertEqual(item.name, 'Hat')
--- myapp/app/users/tests.py
+++ myapp/app/users/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.utils import timezone
+from users.models import CustomUser, Profile
 
-# Create your tests here.
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        CustomUser.objects.create(
+            username='testUser', email='test@email.com', 
+            first_name='test', last_name='user', 
+            password=''
+        )
+
+    def test_model(self):
+        cat = CustomUser.objects.get(id=1)
+        self.assertEqual(cat.first_name, 'test')
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create(username='testUser', email='test@email.com', first_name='test', last_name='user', password='')
+        self.user.set_password('secret')
+        self.user.save()       
+        Profile.objects.create(
+            user=self.user,
+            first_name='Jim',
+            last_name='Halpert', address=123,city='Alabany',
+            postal_code=4999, date_of_birth=timezone.now(), photo=None
+        )
+
+    def test_contact_model(self):
+        item = Profile.objects.get(id=1)
+        self.assertEqual(item.first_name, 'Jim')
